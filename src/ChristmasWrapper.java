aac2fe7e661b6b900f5b9d2a6d076306a0a09242
danielemil19 <daniel.hernandez5@upr.edu>
2019-12-02 18:16:49 -0400

    Added loadToys method (queue and stack practice)

--- src/ChristmasWrapper.java
+++ src/ChristmasWrapper.java
@@ -1,4 +1,5 @@
 import java.util.Comparator;
+import java.util.Queue;
 import java.util.Stack;
 
 public class ChristmasWrapper {
@@ -27,30 +28,38 @@ public class ChristmasWrapper {
 		this.toy = toy;
 		this.named = named;
 	}
-	
+
+	public Toy getToy() {
+		return this.toy;
+	}
+
+	public void setToy(Toy toy) {
+		this.toy = toy;
+	}
+
 	public int getDemand() {
 		return this.toy.demand();
 	}
-	
+
 	public int getPrice() {
 		return this.toy.price();
 	}
-	
+
 	public boolean isNamed() {
 		return this.named;
 	}
-	
+
 	public void setNamed(boolean isNamed) {
 		this.named = isNamed;
 	}
-	
+
 	public boolean equals(Object obj) {
 		if(!(obj instanceof ChristmasWrapper))
 			return false;
 		ChristmasWrapper other = (ChristmasWrapper) obj;
 		return this.getDemand() == other.getDemand() && this.getPrice() == other.getPrice();
 	}
-	
+
 	/**
 	 * A method to return a copy of the given array
 	 * @param deck - Original Array
@@ -76,43 +85,78 @@ public class ChristmasWrapper {
 		 *  The method must be used recursively
 		 *  Hint: Use the helper method below
 		 */
-		
+
 		return null; //Dummy return
 	}
-	
+
 	public static ChristmasWrapper[] priorityToyHelper(Comparator<ChristmasWrapper> comp, ChristmasWrapper[] wishList, ChristmasWrapper first, ChristmasWrapper last) {
 		/*
 		 * TODO Complete the helper method for the priorityToy method
 		 * 
 		 * Hint: Use the comparator to compare the priority between 2 toys
 		 */
-		
+
 		return null; //Dummy return
 	}
-	
-//	public static Stack<ChristmasWrapper> namingFilter(Stack<ChristmasWrapper> bag){
-//		/*
-//		 * TODO Sometimes in a bag of gifts an elf might forget to put the name of the kid the bag is meant for. 
-//		 * Complete this method so that it goes through a given stack, names all the unnamed gifts, and returns the stack 
-//		 * in the same order as it was. Do this recursively using the helper method
-//		 * 
-//		 * 
-//		 * Hint: As you look through a bag, set the toys aside in order to then put them back
-//		 */
-//		
-//		return bag;
-//	}
-//	
-//	public static void namingFilterHelper(Stack<ChristmasWrapper> bag1, Stack<ChristmasWrapper> bag2) {
-//		/*
-//		 * TODO Complete this method to pass toys from one bag to another.
-//		 * 
-//		 * Hint: Visualize as when you check an item then set it aside.
-//		 */
-//	}
-	
-	
-	
+
+	public static Stack<ChristmasWrapper> namingFilter(Stack<ChristmasWrapper> bag){
+		/*
+		 * TODO Sometimes in a bag of gifts an elf might forget to put the name of the kid the bag is meant for. 
+		 * Complete this method so that it goes through a given stack, names all the unnamed gifts, and returns the stack 
+		 * in the same order as it was.
+		 * 
+		 * 
+		 * Hint: As you look through a bag, set the toys aside in order to then put them back
+		 */
+
+		return bag;
+	}
+
+	/**
+	 * @param factoryLine - Production line of toys
+	 * @return stack - Stack representing a new bag of toys for Santa to deliver
+	 */
+	public static Stack<ChristmasWrapper> loadToys(Queue<ChristmasWrapper> factoryLine) {
+		/*
+		 * TODO You have a production line of toys. You want to pass these toys onto
+		 * Santa's bag.
+		 * However, the nerf guns have been found to be defective. If you find one, replace it
+		 * with a watersquirter.
+		 * 
+		 * If the toy is not named, "name" it (set to true) but send it to the back of the line. 
+		 * 
+		 * Repeat this process until the production line is empty
+		 * return the array of stacks created
+		 * 
+		 */
+
+		Stack<ChristmasWrapper> santaBag = new Stack<ChristmasWrapper>();
+
+		while(factoryLine.size() > 0) {
+			ChristmasWrapper toy = factoryLine.poll();
+
+			if (toy.getToy().equals(Toy.NERFGUN)) 
+			{
+				toy.setToy(Toy.WATERSQUIRTER);
+			}
+
+			if (toy.isNamed()) 
+			{
+				santaBag.add(toy);
+			} 
+			else 
+			{
+				toy.setNamed(true);
+				factoryLine.add(toy);
+			}
+		}
+
+
+		return santaBag;
+	}
+
+
+
 	/**
 	 * Comparator for class type card
 	 */
@@ -121,18 +165,18 @@ public class ChristmasWrapper {
 		public ToyComparator() {
 
 		}
-		
+
 		@Override
 		public int compare(ChristmasWrapper first, ChristmasWrapper second) {
 			/* TODO To find a toy with the higher priority, you must compare the relation of (demand / price) and return the difference
-			* 
-			* If the first toy has a higher demand to price ratio, the comparison must return a positive integer, 
-			* 0 if they're the same, and negative otherwise.
-			* 
-			*/
+			 * 
+			 * If the first toy has a higher demand to price ratio, the comparison must return a positive integer, 
+			 * 0 if they're the same, and negative otherwise.
+			 * 
+			 */
 			return -1; //Dummy return
 		}	
 	}
-	
-	
+
+
 }
