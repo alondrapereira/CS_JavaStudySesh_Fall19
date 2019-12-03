c03a6f8307dc6b1c88ebb42fdaac8df86a7fab32
Diego-Paris <45906185+Diego-Paris@users.noreply.github.com>
2019-12-03 01:37:53 -0400

    Added class to use for the in session example

---
+++ src/ClassExample.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
+public class ClassExample 
+{
+	
+	
+	public static void main(String[] args) 
+	{
+		
+		// Create a new ArrayList object
+		
+		
+		// Add, get and remove elements from the ArrayList
+		
+		
+		
+		
+		// Create a new Stack Object
+		
+		
+		// pop, add, and peek elements from the Stack
+		
+		
+		
+		
+		// Create a new Queue Object
+		
+		
+		// poll, add and peek elements from the Queue
+		// Create a PriorityQueue Object
+		
+		
+		
+		
+		// Create a new HashMap Object
+		
+		
+		// put, get and remove elements from the HashMap
+		
+	
+		
+	}
+	
+	
+}
